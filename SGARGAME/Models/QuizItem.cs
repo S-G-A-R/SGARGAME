70ce9f56e7209328b40c0b52d7cbb2fc53b3c05a
Walter Ivan Rosa Sandoval <114608455+IvanRosaSandoval14@users.noreply.github.com>
2025-11-17 11:21:54 -0600

    Categoria3 Test

---
+++ SGARGAME/Models/QuizItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Ivan_SGAR_GAME.Models
+{
+    public class QuizItem
+    {
+        public string QuestionText { get; set; }
+        public List<string> Options { get; set; }
+        // Índice de la opción correcta (0 para la primera, 1 para la segunda, etc.)
+        public int CorrectAnswerIndex { get; set; }
+        public string Explanation { get; set; }
+    }
+}
